d190ea84acacadc8445c3ff7fc8c12f91384de59
vidithj <55511661+vidithj@users.noreply.github.com>
2021-04-08 15:03:18 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module chatmessage
+
+go 1.12
+
+require github.com/gorilla/websocket v1.4.2
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	websocket "chatmessage/websocketpkg"
+	"fmt"
+	"net/http"
+)
+
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("messaging app")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
